7040e98e81c3030df8716a2fbaac0a0b8e24c407
Ivan Valentinov <ivan156drs@gmail.com>
2021-01-11 15:39:17 +0200

    Initial commit

---
+++ src/com/company/ComicBook.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class ComicBook extends Book{
+    private String characterName;
+
+    public ComicBook(double regularPrice, String publisher, int yearPublished, String characterName) {
+        super(regularPrice, publisher, yearPublished);
+        this.characterName = characterName;
+    }
+
+
+    @Override
+    public double computeSalePrice() {
+        return super.getRegularPrice()*0.6;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+}
